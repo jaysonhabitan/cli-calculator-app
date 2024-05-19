3297a5bd99741506d13eac0f862f6c69f3845fee
Jayson Habitan <jayson@treasury.jp>
2024-05-19 16:49:40 +0800

    Add php docs

--- app/Services/Calculator/Operations/OperationInterface.php
+++ app/Services/Calculator/Operations/OperationInterface.php
@@ -4,5 +4,8 @@ namespace App\Services\Calculator\Operations;
 
 interface OperationInterface
 {
+    /**
+     * Computes the given number base on the operation
+     */
     public function compute();
 }
